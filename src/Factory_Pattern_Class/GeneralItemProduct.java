a86a0d6a1d8b34bc94feb59040fd5f1874f463ca
Sihyeon0123 <snc100194@gmail.com>
2023-05-05 16:59:55 +0900

    팩토리 패턴 추상 클래스로 수정
    팩토리 패턴 추상 클래스로 수정

--- src/Factory_Pattern_Class/GeneralItemProduct.java
+++ src/Factory_Pattern_Class/GeneralItemProduct.java
@@ -2,15 +2,7 @@ package Factory_Pattern_Class;
 
 import java.io.File;
 
-public class GeneralItemProduct implements ItemProduct{
-    private String item_title;//물품 명
-    private int item_price;//물품 가격
-    private String item_description;//물품 설명
-    private File item_imageFile;//물품 파일
-    private int item_count;//물품 개수
-    private  String userID;//판매자 명
-    private String type = "일반";//물품 타입
-
+public class GeneralItemProduct extends ItemProduct{
     public GeneralItemProduct(String item_title, int item_price, int item_count, String item_description, File item_imageFile, String userID){
         this.item_title = item_title;
         this.item_price = item_price;
@@ -18,44 +10,6 @@ public class GeneralItemProduct implements ItemProduct{
         this.item_description = item_description;
         this.item_imageFile = item_imageFile;
         this.userID = userID;
-    }
-
-    @Override
-    public String getTitle() {
-        return item_title;
-    }
-
-    @Override
-    public int getPrice() {
-        return item_price;
-    }
-
-    @Override
-    public String getDescription() {
-        return item_description;
-    }
-
-    @Override
-    public File getImageFile() {
-        return item_imageFile;
-    }
-
-    @Override
-    public int getCount() {
-        return item_count;
-    }
-
-    @Override
-    public String getUserID() {
-        return userID;
-    }
-
-    @Override
-    public void decreaseItemCount() {
-        item_count--;
-    }
-    @Override
-    public String getType() {
-        return type;
+        this.item_type = "일반";
     }
 }
--- src/Factory_Pattern_Class/ItemProduct.java
+++ src/Factory_Pattern_Class/ItemProduct.java
@@ -2,21 +2,45 @@ package Factory_Pattern_Class;
 
 import java.io.File;
 
-public interface ItemProduct {
+public abstract class ItemProduct {
+    protected String item_title;//물품 명
+    protected int item_price;//물품 가격
+    protected String item_description;//물품 설명
+    protected File item_imageFile;//물품 파일
+    protected int item_count;//물품 개수
+    protected  String userID;//판매자 명
+    protected String item_type;//물품 타입
+
     /**물품의 이름을 반환*/
-    public String getTitle();
+    public String getTitle(){
+        return  item_title;
+    }
     /**물품의 가격을 반환*/
-    public int getPrice();
+    public int getPrice(){
+        return item_price;
+    }
     /**물품의 설명을 반환*/
-    public String getDescription();
+    public String getDescription(){
+        return item_description;
+    }
     /**물품의 이미지 파일을 반환*/
-    public File getImageFile();
+    public File getImageFile(){
+        return item_imageFile;
+    }
     /**물품의 남은 개수를 반환*/
-    public int getCount();
+    public int getCount(){
+        return item_count;
+    }
     /**유저의 ID을 반환*/
-    public String getUserID();
-    /**물품의 갯수를 1줄인다*/
-    public void decreaseItemCount();
+    public String getUserID(){
+        return userID;
+    }
     /**물품 타입을 반환*/
-    public String getType();
+    public String getItemType(){
+        return item_type;
+    }
+    /**물품의 갯수를 1줄인다*/
+    public void decreaseItemCount(){
+        item_count--;
+    }
 }
