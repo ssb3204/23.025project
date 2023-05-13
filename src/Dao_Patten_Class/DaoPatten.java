893e7c4d3d74358f728d56bf55b5c4ca60219b07
kyungwoo1 <129951563+kyungwoo1@users.noreply.github.com>
2023-05-13 21:47:00 +0900

    다오패턴
    다오패턴 구현 부분

--- src/Dao_Patten_Class/DaoPatten.java
+++ src/Dao_Patten_Class/DaoPatten.java
@@ -1,12 +1,15 @@
 package Dao_Patten_Class;
 
+import java.sql.SQLException;
+
 public interface DaoPatten {
 
-    void addUserinfo(String id, String password, String name, String email);
-    boolean Userinfoexist(String id);
-    void saveUserinfo(String id, String password, String name, String email);
-    boolean checkUserinfo(String id, String password);
-    void deleteUserinfo(String id);
+    void addUserinfo(String id, String password, String name, String email) throws SQLException;
+    Boolean UserinfoExist(String id) throws SQLException;
+    void saveUserinfo(String id, String password, String name, String email) throws SQLException;
+    boolean checkUserinfo(String id, String password) throws SQLException;
+    void deleteUserinfo(String id) throws SQLException;
+
 
 
 }
--- src/Facade_Pattern_Class/DatabaseFacade.java
+++ src/Facade_Pattern_Class/DatabaseFacade.java
@@ -1,10 +1,8 @@
 package Facade_Pattern_Class;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import Dao_Patten_Class.DaoPatten;
+
+import java.sql.*;
 
 public class DatabaseFacade {
     private String url;
@@ -22,6 +20,16 @@ public class DatabaseFacade {
         // 데이터베이스 연결 설정
         connection = DriverManager.getConnection(url, username, password);
     }
+    public Connection connection() {
+        if (connection == null) {
+            try {
+                connect();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return connection;
+    }
 
     public ResultSet executeQuery(String query) throws SQLException {
         // 쿼리 실행 및 결과 반환
@@ -29,4 +37,81 @@ public class DatabaseFacade {
         ResultSet result = statement.executeQuery();
         return result;
     }
+
+    public class UserDAO implements DaoPatten{
+            private DatabaseFacade database;
+
+        public UserDAO(DatabaseFacade database) {
+            this.database = database;
+        }
+            @Override
+            public void addUserinfo(String id, String password, String name, String email) throws SQLException{
+                try (Connection conn = database.connection();
+                     PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (id, password, name, email) VALUES (?, ?, ?, ?)")) {
+                    stmt.setString(1, id);
+                    stmt.setString(2, password);
+                    stmt.setString(3, name);
+                    stmt.setString(4, email);
+                    stmt.executeUpdate();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public Boolean UserinfoExist(String id) throws SQLException{
+                try (Connection conn = database.connection();
+                     PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?")) {
+                    stmt.setString(1, id);
+                    try (ResultSet rs = stmt.executeQuery()) {
+                        if (rs.next()) {
+                            return Boolean.TRUE;
+                        } else {
+                            return Boolean.FALSE;
+                        }
+                    }
+                }
+            }
+
+
+            @Override
+            public void saveUserinfo(String id, String password, String name, String email) throws SQLException {
+                try (Connection conn = database.connection();
+                     PreparedStatement stmt = conn.prepareStatement("UPDATE users SET password = ?, name = ?, email = ? WHERE id = ?")) {
+                    stmt.setString(1, password);
+                    stmt.setString(2, name);
+                    stmt.setString(3, email);
+                    stmt.setString(4, id);
+                    stmt.executeUpdate();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public boolean checkUserinfo(String id, String password) throws SQLException {
+                try (Connection conn = database.connection();
+                     PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE id = ? AND password = ?")) {
+                    stmt.setString(1, id);
+                    stmt.setString(2, password);
+                    try (ResultSet rs = stmt.executeQuery()) {
+                        return rs.next();
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void deleteUserinfo(String id) throws SQLException {
+                try (Connection conn = database.connection();
+                     PreparedStatement stmt = conn.prepareStatement("DELETE FROM users WHERE id = ?")) {
+                    stmt.setString(1, id);
+                    stmt.executeUpdate();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
 }
+
