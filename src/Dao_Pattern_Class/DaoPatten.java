e6ca1aac179e66ad3973951b070d174aee8578b4
kyungwoo1 <129951563+kyungwoo1@users.noreply.github.com>
2023-05-17 11:44:49 +0900

    다오패턴 결합
    다오패턴 결합

---
+++ src/Dao_Pattern_Class/DaoPatten.java
@@ -0,0 +1,15 @@
+package Dao_Pattern_Class;
+
+import java.sql.SQLException;
+
+public interface DaoPatten {
+
+    void addUserinfo(String id, String password, String name, String email) throws SQLException;
+    Boolean UserinfoExist(String id) throws SQLException;
+    void saveUserinfo(String id, String password, String name, String email) throws SQLException;
+    boolean checkUserinfo(String id, String password) throws SQLException;
+    void deleteUserinfo(String id) throws SQLException;
+
+
+
+}
